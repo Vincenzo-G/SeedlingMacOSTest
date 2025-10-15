50d3ea548de57d2e9bac47785e4e3600ea67ef84
Vincenzo-G <vincenzogerelli@gmail.com>
2025-10-15 10:36:12 +0200

    Initial Commit

---
+++ SeedlingMacOSTest/SeedlingMacOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedlingMacOSTestApp.swift
+//  SeedlingMacOSTest
+//
+//  Created by Vincenzo Gerelli on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SeedlingMacOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
